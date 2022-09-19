39323dddf0a46e8a518ae90296e127377eac7b5d
Beckudrup <de3gutter@gmail.com>
2022-09-19 12:39:47 +0200

    Billeder knapper super fedt

--- Assets/Scripts/SceneChanger.cs
+++ Assets/Scripts/SceneChanger.cs
@@ -20,9 +20,7 @@ public class SceneChanger : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //returEgg(int );
-      if(egg=true){
-        Debug.Log("test");
+
 
       }  
     }
--- Assets/endScreen.cs
+++ Assets/endScreen.cs
@@ -8,9 +8,22 @@ public class endScreen : MonoBehaviour
 {
 
     public string[] con1;
+    public string[] con15;
     public string[] con2;
+    public string[] con3;
+    public string[] con4;
+    public string[] con5;
+    public string[] con6;
     public string usertypeIntro;
+   
+    public Image[] meme;
     public TextMeshProUGUI answerDisplay;
+    public TextMeshProUGUI answerDisplay2;
+    public TextMeshProUGUI answerDisplay3;
+    public TextMeshProUGUI answerDisplay4;
+    public TextMeshProUGUI answerDisplay5;
+    public TextMeshProUGUI answerDisplay6;
+
     public TextMeshProUGUI userType;
 
 
@@ -18,11 +31,18 @@ public class endScreen : MonoBehaviour
     void Start()
     {
 
-
-        answerDisplay.text = con2[manager.instance.questionAnswerList[1]];
+        answerDisplay.text = con15[manager.instance.questionAnswerList[1]];
+        
+        answerDisplay2.text = con2[manager.instance.questionAnswerList[2]];
+        
+        answerDisplay3.text = con3[manager.instance.questionAnswerList[3]];
+        answerDisplay4.text = con4[manager.instance.questionAnswerList[4]];
+        answerDisplay5.text = con5[manager.instance.questionAnswerList[5]];
+        answerDisplay6.text = con6[manager.instance.questionAnswerList[6]];
+        
         userType.text = usertypeIntro + con1[manager.instance.questionAnswerList[0]];
-
-
+      
+        //meme.sprite=
 
 
     }
